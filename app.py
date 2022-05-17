b202b188bfd8d403499df6121f5d67e688dd49ac
Manomay Jamble <manomayjamble15@gmail.com>
2022-05-17 23:51:53 +0530

    please work

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask, render_template, request
-import jsonify
-import requests
 import pickle
-import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
-from sympy import re
 app = Flask(__name__)
 model = pickle.load(open('linear_regression.pkl', 'rb'))
 
